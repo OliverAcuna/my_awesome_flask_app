fd30aebcf9f5483b67d4d56afd459bb940436e66
Oliver Acuna <hejnada@outlook.com>
2025-08-25 16:07:11 +0200

    Lägg till Ruff lint CI workflow

---
+++ app.py
@@ -0,0 +1,19 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+
+def hello():
+    return f"Hello, welcome to my flask_app"
+
+@app.route("/bye")
+
+def goodbye():
+    return f"Goodbye my friend"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
+
